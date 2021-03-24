4f73ee51433a208fd0a8ad0d913c9d96bf00e6fb
Eduardo Gonzalez Jr <eddie97g@gmail.com>
2021-03-24 02:57:18 -0500

    converted plain text password in sql script to bcrypt

--- sql scripts/create-database.sql
+++ sql scripts/create-database.sql
@@ -8,13 +8,13 @@ DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student`(
 	`id` int(11) NOT NULL AUTO_INCREMENT,
     `username` varchar(50) UNIQUE NOT NULL,
-    `password` varchar(50) NOT NULL,
+    `password` varchar(100) NOT NULL,
 
     PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 INSERT INTO `student` VALUES
-(1, 'eddieg', 'password');
+(1, 'eddieg', '$2y$12$hSYSttxQX1d/CMMRCzODHu21OZuQm5/erQgtmBm7d/FFBs.X9S5IC');
 
 DROP TABLE IF EXISTS `blacklisted_site`;
 CREATE TABLE `blacklisted_site`(
