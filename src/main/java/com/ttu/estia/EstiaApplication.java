c4735d900eb01d438db98bfed8f66d47cd2ad2e9
Eduardo Gonzalez Jr <eddie97g@gmail.com>
2021-03-24 00:08:04 -0500

    first commit

---
+++ src/main/java/com/ttu/estia/EstiaApplication.java
@@ -0,0 +1,13 @@
+package com.ttu.estia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttu/estia/EstiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttu.estia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
